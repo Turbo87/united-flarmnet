29f9524e7e000c3ad98f90c1a654ef6e5975c19c
Tobias Bieniek <tobias@bieniek.cloud>
2022-09-24 12:14:16 +0200

    Show last update time on webpage

---
+++ build-webpage.mjs
@@ -0,0 +1,17 @@
+import { rm, mkdir, copyFile, readFile, writeFile } from 'node:fs/promises'
+
+async function run () {
+    await rm('dist', { force: true, recursive: true });
+    await mkdir('dist');
+    await copyFile('webpage/background.webp', 'dist/background.webp');
+
+    let html = await readFile('webpage/index.html', 'utf8');
+    let time = new Date().toISOString();
+    let newHtml = html.replaceAll('{{time}}', time);
+    await writeFile('dist/index.html', newHtml);
+
+    await copyFile('united.fln', 'dist/united.fln');
+    await copyFile('united-lx.fln', 'dist/united-lx.fln');
+}
+
+run();
